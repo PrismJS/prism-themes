824621780c6575751b3a418f2336845be9c42184
RunDevelopment <mitchi5000.ms@googlemail.com>
2019-09-04 17:56:26 +0200

    Improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const captureWebsite = require('capture-website');
 
 
 const themesDir = __dirname + '/themes';
 const screenshotDir = __dirname + '/screenshots';
 
-function getThemes() {
-	return fs.readdirSync(themesDir).map(f => (/^.+(?=\.css$)/.exec(f) || [''])[0]).filter(f => f);
+/**
+ * Returns the names of all themes. This includes the `prism-` prefix.
+ */
+async function getThemes() {
+	return (await fs.readdir(themesDir)).map(f => (/^.+(?=\.css$)/.exec(f) || [''])[0]).filter(f => f);
 }
 
 /**
  * Takes a screenshot of all themes overwriting the old ones.
  */
 async function screenshotAllThemes() {
-	for (const theme of getThemes()) {
+	for (const theme of await getThemes()) {
 		await screenshotTheme(theme, true);
 	}
 }
@@ -22,16 +25,25 @@ async function screenshotAllThemes() {
  * Takes a screenshot of themes which don't have one already.
  */
 async function screenshotMissingThemes() {
-	for (const theme of getThemes()) {
+	for (const theme of await getThemes()) {
 		await screenshotTheme(theme, false);
 	}
 }
 
+/**
+ * Takes a screenshot of the given themes and saves the image file in the screenshot directory.
+ *
+ * __IMPORTANT:__ Screenshots have to be taken sequentially, one after an other, to prevent a memory leak.
+ *
+ * @param {string} theme
+ * @param {boolean} overwrite
+ */
 async function screenshotTheme(theme, overwrite) {
 	const file = `${screenshotDir}/${theme}.png`;
-	if (fs.existsSync(file)) {
+
+	if (await fs.stat(file).then(s => s.isFile()).catch(() => false)) {
 		if (overwrite) {
-			fs.unlinkSync(file);
+			await fs.unlink(file);
 		} else {
 			return;
 		}
@@ -42,7 +54,7 @@ async function screenshotTheme(theme, overwrite) {
 		scaleFactor: 1,
 		element: 'pre',
 		styles: [
-			fs.readFileSync(`${themesDir}/${theme}.css`, 'utf-8')
+			await fs.readFile(`${themesDir}/${theme}.css`, 'utf-8')
 		]
 	});
 }
